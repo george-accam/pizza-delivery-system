41f9189025912af7c4d0433cd60f9a06a78bc864
Buertey Accam <manreg814@gmail.com>
2024-09-16 19:47:04 -0500

    update

--- src/screens/user/HomeScreen.js
+++ src/screens/user/HomeScreen.js
@@ -1,45 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Pizzas from "../../components/Pizzas";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { getAllPizzaDataAction } from "../../action/pizzaAction";
 import { otherStuffsAction } from "../../action/otherStuffAction";
-//import Success from "../components/Success";
 import Filter from "../../components/Filter";
 import CartIcon from "../../components/CartIcon";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
+
   const pizzadata = useSelector((state) => state.getAllPizzaDataReducers);
-  const { pizzas, loading, error } = pizzadata;
+  const { pizzas, loading: pizzaLoading, error: pizzaError } = pizzadata;
+
   const otherdata = useSelector((state) => state.otherReducers);
-  //const cartdata = useSelector((state) => state.addToCartReducers);
-  //const { success } = cartdata;
-  const { others } = otherdata;
+  const { others, loading: othersLoading, error: othersError } = otherdata;
+
   useEffect(() => {
     dispatch(getAllPizzaDataAction());
     dispatch(otherStuffsAction());
   }, [dispatch]);
+
   return (
     <div>
       <Filter />
       <div className="row justify-content-center">
-        {loading && (
+        {/* Pizza Loading and Error Handling */}
+        {pizzaLoading && (
           <img
             src="https://i0.wp.com/codemyui.com/wp-content/uploads/2019/01/Rotating-Pizza-Slice-Preloader.gif?w=880&ssl=1"
             alt="loading"
             style={{ width: "100%", height: "100%" }}
           />
         )}
-        {error && (
+        {pizzaError && (
           <img
             src="https://img.freepik.com/premium-vector/pizza-empty-state-error-404_288067-295.jpg"
             alt="error"
             style={{ width: "80%", height: "100%" }}
           />
         )}
-        {!error &&
-          !loading &&
+        {!pizzaError &&
+          !pizzaLoading &&
           pizzas?.length > 0 &&
           pizzas.map((pizza) => (
             <div className="col-md-4" key={pizza._id}>
@@ -48,17 +49,26 @@ export default function HomeScreen() {
           ))}
 
         <hr className="heading-line" />
-        {others &&
-          !error &&
-          !loading &&
-          others?.map((item) => (
+
+        {/* Others Loading and Error Handling */}
+        {othersLoading && <div>Loading other items...</div>}
+        {othersError && <div>Error loading other items!</div>}
+        {!othersError &&
+          !othersLoading &&
+          others?.length > 0 &&
+          others.map((item) => (
             <div className="col-md-4" key={item._id}>
-              <div>
-                <Pizzas pizza={item} />
-              </div>
+              <Pizzas pizza={item} />
             </div>
           ))}
+
+        {/* If no pizzas or others */}
+        {!pizzaLoading &&
+          !othersLoading &&
+          pizzas?.length === 0 &&
+          others?.length === 0 && <div>No data available</div>}
       </div>
+
       <CartIcon />
     </div>
   );
